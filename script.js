136aab35b78fd92d423cd08a255115459903d0bc
Glitch (hello-webpage) <none>
2021-03-20 22:35:11 +0000

    🍨🐧 Checkpoint ./script.js:16231546/774

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector('.location-timezone');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -15,8 +18,13 @@ window.addEventListener("load", () => {
         })
         .then(data => {
           console.log(data);
-          const { temp_f, temp_c, condition } = data.current;
-          console.log(temp_f, temp_c, condition);
+          const { temp_f, temp_c } = data.current;
+          const {text} = data.current.condition;
+          const {name, region} = data.location;
+          // Set DOM Elements from the API
+          temperatureDegree.textContent = temp_f;
+          temperatureDescription.textContent = text;
+          locationTimezone.textContent = name;
         });
     });
   }
