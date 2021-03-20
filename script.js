cd13dd161b842c3b873e565ab866fec80af81552
Glitch (hello-webpage) <none>
2021-03-20 21:59:05 +0000

    🌽🍀 Checkpoint ./script.js:16231546/2834

--- script.js
+++ script.js
@@ -7,17 +7,15 @@ window.addEventListener("load", () => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      //Const proxy = "https://cors-anywhere.herokuapp.com/";
-      //const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ad646a4e53ad6b8ac989cc0f577d0025`;
-      const api = `http://api.weatherapi.com/v1/current.json?key=cbda6a830c834129982214349212003&q=${lat},${long}`;
+      const api = `https://api.weatherapi.com/v1/current.json?key=cbda6a830c834129982214349212003&q=${lat},${long}`;
 
-        
       fetch(api)
         .then(response => {
           return response.json();
         })
         .then(data => {
           console.log(data);
+          const { temp_f, temp_c } = data.current;
         });
     });
   }
