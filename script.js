576c124673d57632aa56b1b43d671df87c7d67e9
Glitch (hello-webpage) <none>
2021-03-20 22:45:00 +0000

    😀🌧 Checkpoint ./script.js:16231546/4608

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
-  let temperatureDescription = document.querySelector('.temperature-description');
-  let temperatureDegree = document.querySelector('.temperature-degree');
-  let locationTimezone = document.querySelector('.location-timezone');
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone = document.querySelector(".location-timezone");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const api = `https://api.weatherapi.com/v1/current.json?key=cbda6a830c834129982214349212003&q=${lat},${long}`;
+      const api = `https://api.weatherapi.com/v1/current.json?key=process.env.API_KEY&q=${lat},${long}`;
 
       fetch(api)
         .then(response => {
           return response.json();
         })
         .then(data => {
-          console.log(data);
           const { temp_f, temp_c } = data.current;
-          const {text} = data.current.condition;
-          const {name, region} = data.location;
+          const { text } = data.current.condition;
+          const { name, region } = data.location;
+
           // Set DOM Elements from the API
           temperatureDegree.textContent = temp_f;
           temperatureDescription.textContent = text;
-          locationTimezone.textContent = name;
+          locationTimezone.textContent = `${name}, ${region}`;
         });
     });
   }
