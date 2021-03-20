d70ef8dbd404a9de3e47dec9220c85d7ac090e7c
Glitch (hello-webpage) <none>
2021-03-20 21:11:16 +0000

    🗾🐺 Checkpoint ./script.js:16231546/177

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ window.addEventListener('load', () => {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
+      
+      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid={ad646a4e53ad6b8ac989cc0f577d0025
+}`;
     })
   }
 });
