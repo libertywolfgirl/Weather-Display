652fec235c995fe9bcc41bfc22cc9ab26ed6d209
Glitch (hello-webpage) <none>
2021-03-21 02:50:02 +0000

    🐡🙇 Checkpoint ./script.js:16231546/372 ./index.html:16231546/179

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener("load", () => {
     ".temperature-description"
   );
   let temperatureDegree = document.querySelector(".temperature-degree");
+  let feelsLikeDegree = document.querySelector(".feels-like-degree");
   let locationTimezone = document.querySelector(".location-timezone");
   let image = document.createElement("img");
   let temperatureSection = document.querySelector(".temperature");
@@ -23,13 +24,14 @@ window.addEventListener("load", () => {
         })
         .then(data => {
           console.log(data);
-          const { temp_f, temp_c } = data.current;
+          const { temp_f, temp_c, feelslike_c, feelslike_f } = data.current;
           const { text, icon } = data.current.condition;
           const { name, region } = data.location;
 
           // Set DOM Elements from the API
           temperatureDegree.textContent = temp_f;
           temperatureDescription.textContent = text;
+          feelsLikeDegree.textContent = feelslike_f;
           locationTimezone.textContent = `${name}, ${region}`;
 
           // Set Icon
@@ -41,9 +43,11 @@ window.addEventListener("load", () => {
             if (temperatureSpan.textContent === "F") {
               temperatureSpan.textContent = "C";
               temperatureDegree.textContent = temp_c;
-            } else if (temperatureSpan.textContent === "C") {
+              feelsLikeDegree.textContent = feelslike_c;
+            } else {
               temperatureSpan.textContent = "F";
               temperatureDegree.textContent = temp_f;
+              feelsLikeDegree.textContent = feelslike_f;
             }
           });
         });
